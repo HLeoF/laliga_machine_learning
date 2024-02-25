da5d3f3de18b625f6777b536659595db637a9884
Maiqi Hou <Maiqi_Hou1@baylor.edu>
2024-02-24 20:58:25 -0700

    update-info

---
+++ laligaProject/clustering/K-means.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+
+clustering_df = pd.read_csv("clustering_analysis_df.csv")
+
+######################### K-Means Clustering ##############
+
+X = clustering_df[['shots_total','shots_on','goals_total']]
+scaler = StandardScaler()
+scale_X = scaler.fit_transform(X)
+
+######################### K Value Selection ###############
+wcss = []
+score = [0.0]
+
+#### Elbow Method #########################################
+for i in range(1, 11):
+  model = KMeans(n_clusters=i)
+  labels = model.fit_predict(scale_X)
+  wcss.append(model.inertia_)
+
+########## Silhouette Method #############################
+for i in range(2, 11):
+  model = KMeans(n_clusters=i)
+  labels = model.fit_predict(scale_X)
+  scores = silhouette_score(scale_X,model.labels_)
+  score.append(scores)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+ax1.plot(range(1, 11), wcss, marker='o')
+ax1.axvline(x=3, color='r', linestyle='--')
+ax1.set_xlabel('Number of clusters K')
+ax1.set_ylabel('WCSS')
+ax1.set_title('Elbow Method')
+
+ax2.plot(range(1, 11), score, marker='o')
+ax2.axvline(x=2, color='r', linestyle='--')
+ax2.set_xlabel('Number of clusters K')
+ax2.set_ylabel('Silhouette Score')
+ax2.set_title('Silhouette Method')
+
+plt.tight_layout()
+plt.show()
+
+############# K-Mean Visualization ###########################
+df2 = clustering_df.copy()
+model2 = KMeans(n_clusters=2)
+labels2 = model2.fit_predict(scale_X)
+df2["cluster"] = labels2
+
+df3 = clustering_df.copy()
+model3 = KMeans(n_clusters=3)
+labels3 = model3.fit_predict(scale_X)
+df3["cluster"] = labels3
+
+df4 = clustering_df.copy()
+model4 = KMeans(n_clusters=4)
+labels4 = model4.fit_predict(scale_X)
+df4["cluster"] = labels4
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+
+ax1.scatter(df2['shots_total'],df2['shots_on'],c = df2['cluster'])
+ax1.set_xlabel("Shots Total")
+ax1.set_ylabel("Shots On")
+ax1.set_title("K Value = 2 & Silhouette Method")
+
+ax2.scatter(df3['shots_total'],df3['shots_on'],c = df3['cluster'])
+ax2.set_xlabel("Shots Total")
+ax2.set_ylabel("Shots On")
+ax2.set_title("K Value = 3 & Elbow Method")
+
+ax3.scatter(df4['shots_total'],df4['shots_on'],c = df4['cluster'])
+ax3.set_xlabel("Shots Total")
+ax3.set_ylabel("Shots On")
+ax3.set_title("K Value = 4")
+
+plt.tight_layout()
+plt.show()
---
+++ laligaProject/clustering/clustering_pre_data.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import statistics as stat
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+
+################ Data Transforming ###########################
+player = pd.read_csv("clean_laliga_playerDF.csv")
+club = pd.read_csv("clean_laliga_teamDF.csv")
+
+df = player[['season','teamName','playerName','position','shots_total','shots_on','goals_total']]
+df = df[~df['position'].str.contains("Goalkeeper")] #门将不在考虑范围内，需要去掉
+df = df[['season','teamName','playerName','shots_total','shots_on','goals_total']]
+print(df.head(5))
+
+club = club[["season","teamName","win_home","win_away","draw_home","draw_away","lose_home","lose_away"]]
+club.loc[:, 'total_win'] = club['win_home'] + club['win_away']
+club.loc[:, 'total_draw'] = club['draw_home'] + club['draw_away']
+club.loc[:, "PTS"] = (club["total_win"]*3) + club["total_draw"]
+club = club[["season","teamName","PTS"]]
+print(club.head(5))
+
+clustering_df = pd.merge(df, club, on=['season', 'teamName'])
+
+print(clustering_df.dtypes)
+
+clustering_df.to_csv("clustering_analysis_df.csv")

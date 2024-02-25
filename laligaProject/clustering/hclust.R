32fa526f2c5ba6c59b28f6f84c09acb9b762e88b
Maiqi Hou <Maiqi_Hou1@baylor.edu>
2024-02-25 15:22:05 -0700

    update

--- laligaProject/clustering/hclust.R
+++ laligaProject/clustering/hclust.R
@@ -3,27 +3,56 @@ install.packages("factoextra")
 library(tidyverse)
 library(cluster)
 library(factoextra)
+library(stylo) ## for using dis cosine
 
-
-library(stylo)
-
+# Set the absolute file path
 setwd("D:/PycharmProjects/pythonProject/laliga_machine_learning/laligaProject/clustering")
 
+#Read the csv file
 hclust_df <- read.csv('clustering_analysis_df.csv')
 
+#Select shots_total, shots_on, and goals_total variables
 data <- hclust_df[, 5:7]
-data_scl <- scale(data)
-dist_df <- stylo::dist.cosine(data_scl)
-hc <- hclust(dist_df, method = "complete")
-plot(hc, cex = 0.1, hang = -1000, main = "Cosine",labels = hclust_df$playerName)
+data_scl <- scale(data) # scale data
+dist_df <- stylo::dist.cosine(data_scl) # compute cosine Cosine Similarity
+hc <- hclust(dist_df, method = "complete") # using cop
+plot(hc, cex = 0.1, hang = -1, main = "Cosine Similarity",labels = hclust_df$playerName)
+rect.hclust(hc, k = 3, border = 2:5)
 
-cut_height <- 1.3
-cutree_result <- cutree(hc, h = cut_height)
-clipped_data <- data[cutree_result == 1, ]
 
-clipped_dist <- stylo::dist.cosine(scale(clipped_data))
-clipped_hc <- hclust(clipped_dist, method = "complete")
 
-l <- hclust_df$playerName[cutree_result == 1]
+tree_height <-1.9 # cut tree height from the original hclust
+clus <- cutree(hc, h = tree_height)
+temp <- data[clus == 1, ]
 
-plot(clipped_hc, cex = 0.5, hang = -30, main = "Cosine", labels = l)
+temp_dist <- stylo::dist.cosine(scale(temp)) 
+temp_hc <- hclust(temp_dist, method = "complete")
+
+l <- hclust_df$playerName[clus == 1]
+
+plot(temp_hc, cex = 0.7, hang = -30, main = "Cosine Similarity", labels = l)
+rect.hclust(temp_hc, k = 2, border = 2:5)
+
+df <- paste( hclust_df$teamName[clus == 2], hclust_df$playerName[clus == 2], hclust_df$season[clus == 2],sep ='-')
+df <- data.frame(df)
+colnames(df)[1] <- "info"
+df <- df[grepl("2020", df$info) & (grepl("Barcelona|Atletico Madrid|Real Madrid", df$info)), ]
+print(df)
+
+
+df1 <- paste( hclust_df$teamName[clus == 3], hclust_df$playerName[clus == 3], hclust_df$season[clus == 3],sep ='-')
+df1 <- data.frame(df1)
+colnames(df1)[1] <- "info"
+df1 <- df1[grepl("2020", df1$info) & (grepl("Barcelona|Atletico Madrid|Real Madrid", df1$info)), ]
+print(df1)
+
+
+df2 <- paste( hclust_df$teamName[clus == 1], hclust_df$playerName[clus == 1], hclust_df$season[clus == 1],sep ='-')
+df2 <- data.frame(df2)
+colnames(df2)[1] <- "info"
+df2 <- df2[grepl("2020", df2$info) & (grepl("Barcelona|Atletico Madrid|Real Madrid", df2$info)), ]
+print(df2)
+
+
+
+

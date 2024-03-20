5f2ddfeb6e8611e6134d9c07365de1f5a897ab88
Maiqi Hou <Maiqi_Hou1@baylor.edu>
2024-03-20 15:33:32 -0600

    Train set and Test set

--- laligaProject/NB/Multinomial NB Model.py
+++ laligaProject/NB/Multinomial NB Model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix

7e47ec1406a71183adf14976c0eba76a4c9fbb8c
Maiqi Hou <Maiqi_Hou1@baylor.edu>
2024-04-22 13:55:46 -0600

    SVM_update

--- laligaProject/SVM/SVM_pre_data.py
+++ laligaProject/SVM/SVM_pre_data.py
@@ -5,32 +5,41 @@ from sklearn.model_selection import train_test_split
 
 ########################## Pre-process Dataset from the original dataset #######################
 
-df = pd.read_csv("../SVM/clean_laliga_playerDF.csv")
+df = pd.read_csv("clean_laliga_playerDF.csv")
 
-player = df[['season','teamName','playerName','position','pass_accura','tackle_blocks','tackle_intercep','fouls_committed']]
+
+df['duel_success_rate'] = df['duel_won'] / df['duel_total']
+df['dribble_success_rate'] = df['dribble_success'] / df['dribble_attemp']
+player = df[['season', 'teamName', 'playerName', 'position', 'rating',
+             'game_minutes', 'duel_success_rate', 'dribble_success_rate',
+             'key_pass', 'pass_accura', 'fouls_drawn']]
 player = player[player['season'].isin([2020, 2021, 2022])]
-player = player[player['position'] == "Defender"]
-player = player[~((player[['pass_accura','tackle_blocks','tackle_intercep','fouls_committed']] == 0).all(axis=1))]
+player = player[player['position'] == "Midfielder"]
+player = player.dropna()
+
+
 print(player)
 
 ########################Using K-means clustering to label categories######################################
-X = player[['pass_accura','tackle_blocks','tackle_intercep','fouls_committed']]
+
+X = player[['rating','game_minutes','duel_success_rate', 'dribble_success_rate','key_pass', 'pass_accura', 'fouls_drawn']]
 scaler = StandardScaler()
 scale_X = scaler.fit_transform(X)
-k_means_model = KMeans(n_clusters=3)
+k_means_model = KMeans(n_clusters=2)
 labels = k_means_model.fit_predict(X)
 player["performance"] = labels
 
+
 def cluster_value(value):
-    if value == 2:
+    if value == 0:
         return 'Good'
     elif value == 1:
         return "Normal"
-    elif value == 0:
-        return "Bad"
 
 player['performance'] =  player['performance'].apply(cluster_value)
 player['performance'] = player['performance'].astype('category')
+
+print(player)
 
 ####################### Split dataset to Train set and Test set#################################
 playerTrainDF, playerTestDF = train_test_split(player,test_size = 0.3, random_state=42)
